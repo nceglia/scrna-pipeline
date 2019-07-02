393458e6adcee5b3764b45e5f3b427ec2dfaacdc
Nick Ceglia <nickceglia@gmail.com>
2019-07-02 16:01:28 -0400

    runrdata

--- workflows/run_collection.py
+++ workflows/run_collection.py
@@ -23,21 +23,17 @@ def RunDownload(sampleids, finished):
         path_json = {sample: path}
         open(finished(sample),"w").write(json.dumps(path_json))
 
-def RunRdata(sampleid, finished):
-    print("Getting Collection.")
-    sampleids = open(config.samples, "r").read().splitlines()
-    tenx_collection = SampleCollection(sampleids)
-    open("sample_paths.json","w").write(json.dumps(tenx_str))
-    # for sampleid, tenx_path in tenx_collection:
-    #     tenx_analysis = TenxAnalysis(tenx_path)
-    #     tenx_analysis.load()
-    #     tenx_analysis.extract()
-    #     print("Extracted.")
-    #     qc = QualityControl(tenx_analysis,sampleid)
-    #     if not os.path.exists(qc.sce):
-    #         qc.run(mito=config.mito)
-    #         print ("Uploading")
-    open(finished,"w").write("Completed")
+def RunRdata(sample_to_path, finished):
+    sample = json.loads(open(sample_to_path,"r").read())
+    sampleid, path = sample.items().pop()
+    tenx_analysis = TenxAnalysis(path)
+    tenx_analysis.load()
+    tenx_analysis.extract()
+    print("Extracted.")
+    # qc = QualityControl(tenx_analysis,sampleid)
+    # if not os.path.exists(qc.sce):
+    #     qc.run(mito=config.mito)
+
 
 
 def RunCollection(workflow):
@@ -49,13 +45,14 @@ def RunCollection(workflow):
             pypeliner.managed.TempOutputFile("sample_path.json","sample")
         )
     )
-    # workflow.transform (
-    #     name = "process_rdata",
-    #     func = RunDownload,
-    #     args = (
-    #         open(config.samples, "r").read().splitlines(),
-    #         pypeliner.managed.TempOutputFile("sample_path.json","sample")
-    #     )
-    # )
+    workflow.transform (
+        name = "extract_rdata",
+        func = RunDownload,
+        axes = ('sample',),
+        args = (
+            pypeliner.managed.TempInputFile("sample_path.json","sample")
+            pypeliner.managed.OutputFile("sample.rdata","sample")
+        )
+    )
 
     return workflow
