70e3f1f75d855f3fedf6ec00270466919759a8af
Nick Ceglia <nickceglia@gmail.com>
2019-07-03 11:54:30 -0400

    runrdata

--- workflows/run_collection.py
+++ workflows/run_collection.py
@@ -41,7 +41,7 @@ def RunExtract(sample_to_path, rdata_path):
 def RunCellAssign(sce, annot_sce):
     sampleid = sce.split("/")[-2]
     print("sample",sampleid)
-    # CellAssign.run(sampleid, config.rho_matrix, sce)
+    CellAssign.run(sampleid, config.rho_matrix, sce)
     filtered_sce = os.path.join(os.path.split(qc.sce)[0],"sce_cas.rdata")
     shutil.copyfile(filtered_sce, annot_sce)
 
@@ -97,15 +97,15 @@ def RunCollection(workflow):
         )
     )
 
-    workflow.transform (
-        name = "run_convert",
-        func = RunConvert,
-        axes = ('sample',),
-        args = (
-            pypeliner.managed.InputFile("sce.rdata","sample"),
-            pypeliner.managed.OutputFile("seurat.rdata","sample"),
-        )
-    )
+    # workflow.transform (
+    #     name = "run_convert",
+    #     func = RunConvert,
+    #     axes = ('sample',),
+    #     args = (
+    #         pypeliner.managed.InputFile("sce.rdata","sample"),
+    #         pypeliner.managed.OutputFile("seurat.rdata","sample"),
+    #     )
+    # )
 
 
     return workflow
