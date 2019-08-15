9e2eec92ed4a7b6a16017fa5c2c6ec28e6285db9
Nick Ceglia <nickceglia@gmail.com>
2019-08-15 14:45:51 -0400

    clonealign pipeline

--- workflows/run_clonealign.py
+++ workflows/run_clonealign.py
@@ -7,6 +7,7 @@ import os
 import json
 import shutil
 import subprocess
+from collections import defaultdict
 
 from interface.tenxanalysis import TenxAnalysis
 from utils.cloud import TenxDataStorage
