50601b215083495282da4a85dd70c2663ddbcb3f
Nick Ceglia <nickceglia@gmail.com>
2019-08-20 15:34:00 -0400

    adding qplot and removing outlying

--- workflows/run_clonealign.py
+++ workflows/run_clonealign.py
@@ -174,6 +174,8 @@ def RunEvaluation(annotated_sce, cal_fit, cnv_mat, evaluate_png):
     cnv <- readRDS('{cnv_mat}')""".format(annotated_sce=annotated_sce,cnv_mat=cnv_mat,cal_fit=cal_fit)
 
     rcode += """
+    write.csv(cnv, file = "cnv.csv")
+    raw_cnvs <- fread("cnv.csv")
     recompute_clone_assignment <- function(ca, clone_assignment_probability = 0.95) {
       clone_names <- colnames(ca$ml_params$clone_probs)
       clones <- apply(ca$ml_params$clone_probs, 1, function(r) {
