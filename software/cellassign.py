319722af78f1f02d4b4cbadb75e79b8ab467e4d0
Nick Ceglia <nickceglia@gmail.com>
2019-09-11 14:02:19 -0400

    remapped

--- software/cellassign.py
+++ software/cellassign.py
@@ -15,14 +15,14 @@ class CellAssign(object):
         env = os.environ.copy()
         submit = ["Rscript","{}/run_cellassign.R".format(os.path.split(rdata)[0])]
         if lsf:
-            submit = """singularity exec --bind /admin --bind $(pwd):$(pwd) /home/ceglian/images/scrna-pipeline-v3.img /admin/lsf/10.1/linux3.10-glibc2.17-x86_64/bin/bsub -K -J cellassign -R rusage[mem=1] -n 20 -We 40 -o out -e err /home/ceglian/anaconda/bin/Rscript {}""".format("{}/run_cellassign.R".format(os.path.split(rdata)[0])).split()
+            submit = """/admin/lsf/10.1/linux3.10-glibc2.17-x86_64/bin/bsub -K -J cellassign -R rusage[mem=1] -n 20 -We 40 -o out -e err singularity exec --bind /admin --bind $(pwd):$(pwd) /home/ceglian/images/scrna-pipeline-v3.img Rscript {}""".format("{}/run_cellassign.R".format(os.path.split(rdata)[0])).split()
         else:
             submit = ["Rscript","{}/run_cellassign.R".format(os.path.split(rdata)[0])]
             print(submit)
         subprocess.call(submit, env=env)
         matched_results = os.path.join(os.path.split(results)[0],"cell_types.tsv")
         if lsf:
-            submit = """singularity exec --bind /admin --bind $(pwd):$(pwd) /home/ceglian/images/scrna-pipeline-v3.img /admin/lsf/10.1/linux3.10-glibc2.17-x86_64/bin/bsub -K -J mapcells -R rusage[mem=1] -n 20 -We 40 -o out -e err /home/ceglian/anaconda/bin/Rscript {}""".format("{}/match.R".format(os.path.split(rdata)[0])).split()
+            submit = """/admin/lsf/10.1/linux3.10-glibc2.17-x86_64/bin/bsub -K -J mapcells -R rusage[mem=1] -n 2 -We 5 -o out -e err singularity exec --bind /admin --bind $(pwd):$(pwd) /home/ceglian/images/scrna-pipeline-v3.img Rscript {}""".format("{}/match.R".format(os.path.split(rdata)[0])).split()
             print(submit)
         else:
             submit = ["Rscript","{}/match.R".format(os.path.split(rdata)[0])]
