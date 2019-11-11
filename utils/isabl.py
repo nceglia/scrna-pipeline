afcd36d73528903ce522259c5f014905d81548fc
Nick Ceglia <nickceglia@gmail.com>
2019-11-11 12:20:30 -0500

    testing storage

--- utils/isabl.py
+++ utils/isabl.py
@@ -16,19 +16,19 @@ class TenxDataStorage(object):
     def __init__(self, sampleid):
         self.sampleid = sampleid.replace(".tar.gz","")
         self.tenx_path = None
-        # self.cache = ".cache"
-        # try:
-        #     os.makedirs(self.cache)
-        # except Exception as e:
-        #     pass
+        self.cache = ".cache"
+        try:
+            os.makedirs(self.cache)
+        except Exception as e:
+            pass
 
-    def retrieve_latest(self):
+    def download(self):
         path_template = "{0}/outs/"
         sample = metadata.single_sample(self.sampleid)
         analyses = isabl_cli.get_analyses()
-        analysis_stack = collections.defaultdict(list)
-
+        analysis_stack = list()
         unique_id = sample["unique_id"]
+
         experiments = isabl_cli.get_experiments(sample__identifier=unique_id)
         for analysis in analyses:
             system_id = analysis.__dict__["targets"][0]["system_id"]
@@ -37,27 +37,20 @@ class TenxDataStorage(object):
                     continue
                 if "CELLRANGER" != analysis["application"].__dict__["name"]:
                     continue
-                print("Cellranger")
-                print(analysis["status"])
                 if analysis["status"] == "SUCCEEDED":
-                    print("sucess")
-                    analysis_stack[unique_id].append(analysis["storage_url"])
-                    print("hit")
+                    analysis_stack.append(analysis["storage_url"])
 
-        paths = dict()
-        for sample, urls in analysis_stack.items():
+        while len(analysis_stack) != 0:
             latest = urls.pop()
-            cellranger_outs = os.path.join(latest, path_template.format(sample))
+            cellranger_outs = os.path.join(latest, path_template.format(unique_id))
             if os.path.exists(cellranger_outs):
-                print(cellranger_outs)
+                self.tenx_path = os.path.join(self.cache,unique_id)
+                shutil.copytree(cellranger_outs, self.tenx_path)
                 break
-
-        # return paths
-        #
-        # self.tenx_path = os.path.join(self.cache,self.sampleid)
-        # return self.tenx_path
+        return self.tenx_path
 
 
 if __name__ == '__main__':
     storage = TenxDataStorage("SPECTRUM-OV-052_S1_CD45N_INFRACOLIC_OMENTUM")
-    storage.retrieve_latest()
+    storage.download()
+    print(storage.tenx_path)
