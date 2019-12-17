792f0bd445fc3ba5214da7258536d42af10cb935
Nick Ceglia <nickceglia@gmail.com>
2019-12-17 17:15:52 -0500

    adding db stub

---
+++ interface/db.py
@@ -0,0 +1,16 @@
+import os
+import pandas
+import numpy
+
+
+class Panglao(object):
+
+    def __init__(self):
+        pass
+
+    def celltypes(self):
+
+class CellMarker(object):
+
+    def __init__(self):
+        pass
