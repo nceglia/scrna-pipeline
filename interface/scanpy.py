5f7da9953a1ce822ffeab94b0d80bd7513d3e477
Nick Ceglia <nickceglia@gmail.com>
2019-12-17 17:16:37 -0500

    scanpy stub

---
+++ interface/scanpy.py
@@ -0,0 +1,13 @@
+import numpy
+import pandas
+
+
+class Scanpy(object):
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def fromRData(scanpy_class, rdata):
+        scanpy = Scanpy()
+        return scanpy
