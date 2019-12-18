ed49a7770cb4c4ffbe976d7c78c98416c6bffd7a
nceglia <nickceglia@gmail.com>
2019-12-18 20:45:30 +0000

    efkln

--- interface/scanpy.py
+++ interface/scanpy.py
@@ -12,20 +12,31 @@ class Scanpy(object):
     @classmethod
     def fromRData(scanpy_class, rdata):
        
+        if not rdata: raise Exception
+
         sce = SingleCellExperiment.fromRData(rdata)
-       
+        print( sce.assays.keys())
         counts = sce.assays["counts"]
+        log_counts = sce.assays["logcounts"]
+
         row_data = pd.DataFrame(sce.rowData)
         col_data = pd.DataFrame(sce.colData)
-        annobj = anndata.AnnData(X = counts, obs = row_data, var = col_data )
 
-        scanpy = Scanpy(annobj)
+        counts_annobj = anndata.AnnData(X = counts, obs = row_data, var = col_data )
+        logcounts_annobj = anndata.AnnData(X = log_counts, obs = row_data, var = col_data )
+        scanpy = Scanpy(annobjannobj, logcounts_annobj)
         return scanpy
 
-    def get_counts  
+    def get_counts():
+        return self.counts.X
+    
+    def get_norm_counts():
+        return self.norm_counts.X
+    
+
     def to_sce(self):
         return None
 
 scanpy1 = Scanpy.fromRData("/home/abramsd/hack/hackathon_scrna/data/lung_1.rdata")
-
+scanpy1.get_counts()
 
