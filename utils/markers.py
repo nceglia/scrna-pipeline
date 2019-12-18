bd60292237b8e35407d6af38315b36609609d582
redpanda-cat <leungs1@mskcc.org>
2019-12-18 16:37:51 -0500

    Add stubs

---
+++ utils/markers.py
@@ -0,0 +1,13 @@
+def MarkerStatistics(scanpies, marker_matrix):
+    # return pandas dataframe with statistics of normalized expression across cells for each marker gene (mean, median, std) by dataset and across datasets.
+    pass
+
+
+def MarkerUmaps(scanpies, gene_matrix, filename):
+    # umap for each sample with expression of marker gene for each gene
+    pass
+
+
+def MarkerDot(scanpies, gene_matrix, filename):
+    # dot plot matrix for each cell type and marker gene (see method "rank_genes_groups_dotplot") per dataset and across datasets
+    pass
