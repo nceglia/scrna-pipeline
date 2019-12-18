f7d874c7618054a2c184fb63c3f9bb7df42d4b8d
Douglas Abrams <abramsd@mskcc.org>
2019-12-18 13:11:25 -0500

    added parser for cellMarker

--- interface/db.py
+++ interface/db.py
@@ -6,8 +6,8 @@ import numpy
 class Panglao(object):
 
     def __init__(self, file):
-        csv = pd.read_csv(file, sep="\t")
-        self.data = csv
+        self.data = pd.read_csv(file, sep="\t")
+        
 
     def celltypes(self, tissue=None):
 
@@ -42,18 +42,72 @@ class Panglao(object):
 
 class CellMarker(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, file):
+        self.data = pd.read_csv(file, sep = "\t")
 
 
-panglao = Panglao(file='../downloads/PanglaoDB_markers_17_Dec_2019.tsv')
-# all_celltypes = panglao.celltypes()
-# print(all_celltypes)
+    def celltypes(self, tissue=None):
 
-lung_celltypes = panglao.celltypes(tissue="Lungs")
-print(lung_celltypes)
-matrix = panglao.marker_matrix(lung_celltypes)
-print(matrix)
+        tissues = self.get_tissues()
+        if tissue:
+            #tissue = tissue.lower()
+            assert tissue in tissues, tissue + \
+                " does not exist - please pick from: " + " ".join(tissues)
+            records = self.data[self.data["tissueType"] == tissue]
+        else:
+            records = self.data
+
+        return records["cellName"].unique()
+
+    def marker_matrix(self, celltypes, species=None):
+        marker_dict = {}
+        records = self.data[self.data["speciesType"]
+                            == species] if species else self.data
+
+        for celltype in celltypes:
+            celltype_records = records.loc[records["cellName"] == celltype]
+            marker_genes = celltype_records["geneSymbol"].unique(
+            ).tolist()
+            marker_dict[celltype] = marker_genes
+
+        return marker_dict
+
+    def get_tissues(self):
+        return [tissue
+                for tissue in self.data["tissueType"].unique() if not isinstance(tissue, float)]
+
+
+
+
+def test_cellMarker():
+    cm = CellMarker(file='/Users/abramsd/HACK/cellMarkers.txt')
+
+    all_celltypes = cm.celltypes()
+    #print(all_celltypes)
+
+    lung_celltypes = cm.celltypes(tissue="Kidney")
+    print(lung_celltypes)
+    matrix = cm.marker_matrix(lung_celltypes)
+    print(matrix)
+
+
+    # # lung_celltypes2 = cm.celltypes(tissue="lungs")
+    # # print(lung_celltypes)
+
+    # abc_celltypes = cm.celltypes(tissue="abc")
+    # print(abc_celltypes)
+
+
+test_cellMarker()
+
+# panglao = Panglao(file='/Users/abramsd/HACK/cellMarkers.txt')
+# # all_celltypes = panglao.celltypes()
+# # print(all_celltypes)
+
+# lung_celltypes = panglao.celltypes(tissue="Lungs")
+# print(lung_celltypes)
+# matrix = panglao.marker_matrix(lung_celltypes)
+# print(matrix)
 
 
 # lung_celltypes2 = panglao.celltypes(tissue="lungs")
